3128806fae5d61a3bf03fb9a3a6f242f974d216d
Kremmen <you@example.com>
2026-03-19 09:42:02 +1000

    generate schedules before calculating interest

--- src/books.rs
+++ src/books.rs
@@ -904,11 +904,11 @@ impl Books {
 
     pub fn run_checks_and_update(&mut self, projection_date: NaiveDate) -> Result<(), BooksError>{
         println!("Running checks 📋  Projection date: {}", projection_date);
-        let interest_accounts = self.accounts.values().filter(|a| a.interest_id.is_some()).cloned().collect();
-        println!("Calculating interest...");
-        calculate_interest_for_accounts(self, interest_accounts, projection_date)?;                
         println!("Generating schedules...");
         self.generate(projection_date);
+        let interest_accounts = self.accounts.values().filter(|a| a.interest_id.is_some()).cloned().collect();
+        println!("Calculating interest...");
+        calculate_interest_for_accounts(self, interest_accounts, projection_date)?;
         println!("Checks completed ✅");
         Ok(())
     }
